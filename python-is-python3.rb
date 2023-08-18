8841e4ebd5f72b719bbb0771ad79dbdc6b179c07
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2023-08-18 15:21:00 -0400

    added version

--- python-is-python3.rb
+++ python-is-python3.rb
@@ -2,6 +2,7 @@ class PythonIsPython3 < Formula
   desc "python3 to python symlink"
   homepage ""
   url "https://raw.githubusercontent.com/octoturt/homebrew-repository/main/empty.tar.gz"
+  version "v1.0.0"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license ""
 
