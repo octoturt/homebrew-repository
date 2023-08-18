903ba9e1af458c77221cfb7706190a9d83591e60
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2023-08-18 15:59:08 -0400

    help

--- python-is-python3.rb
+++ python-is-python3.rb
@@ -3,13 +3,12 @@ class PythonIsPython3 < Formula
   homepage ""
   url "https://raw.githubusercontent.com/octoturt/homebrew-repository/main/python-is-python3-1.0.0.tar.gz"
   version "v1.0.0"
-  sha256 "5187aa519a500957739edeb6b7c2b939252a2830cc3bc1eb3f78908b52988f01"
+  sha256 "eadb5db35836c0eb9d31ce6e4b64fe8abf6077ad800cce6c7ae1e2bd4b8e2ead"
   license ""
 
   # depends_on "cmake" => :build
 
   def install
-    system "/bin/ln -s #{HOMEBREW_PREFIX}/bin/python3 #{prefix}/bin/python"
     bin.install "python"
   end
 end
