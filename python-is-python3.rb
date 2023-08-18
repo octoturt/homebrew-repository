560ee6475337900ced03765b4cb7f22acd65be3e
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2023-08-18 15:42:20 -0400

    i think this is the one

--- python-is-python3.rb
+++ python-is-python3.rb
@@ -1,9 +1,9 @@
 class PythonIsPython3 < Formula
   desc "python3 to python symlink"
   homepage ""
-  url "https://raw.githubusercontent.com/octoturt/homebrew-repository/main/empty.tar.gz"
+  url "https://raw.githubusercontent.com/octoturt/homebrew-repository/main/python-is-python3-1.0.0.tar.gz"
   version "v1.0.0"
-  sha256 "11e00a6889de11444549026680fe5fd48d06fe9865442da9edad5483528ec915"
+  sha256 "5187aa519a500957739edeb6b7c2b939252a2830cc3bc1eb3f78908b52988f01"
   license ""
 
   # depends_on "cmake" => :build
