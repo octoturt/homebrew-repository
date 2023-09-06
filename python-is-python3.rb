539c8ee8673a80441da0b5d5d3d09097ba58952d
Alexis Scobey <19824067+octoturt@users.noreply.github.com>
2023-09-06 17:51:28 -0400

    update python-is-python3 to v1.0.1

--- python-is-python3.rb
+++ python-is-python3.rb
@@ -1,9 +1,9 @@
 class PythonIsPython3 < Formula
   desc "python3 to python symlink"
   homepage ""
-  url "https://github.com/octoturt/python-is-python3/archive/refs/tags/v1.0.0.tar.gz"
-  version "v1.0.0"
-  sha256 "9ea08f854dcbe24dac92aaf0eba0237e2be8a86536196ebfe8f0a68d3b9e1265"
+  url "https://github.com/octoturt/python-is-python3/archive/refs/tags/v1.0.1.tar.gz"
+  version "v1.0.1"
+  sha256 "2c592f5619a22213361a7da76864ac332249cf94be886e69c987e9312d7d6e24"
   license ""
 
   depends_on "python3"
@@ -11,5 +11,6 @@ class PythonIsPython3 < Formula
   def install
     system "/bin/bash ./linker.sh"
     bin.install "python"
+    bin.install "pip"
   end
 end
